6f4f875e9c26cd930b1421f0341351953115bf31
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-03-05 12:06:38 -0600

    hat2

--- a1/sketch.js
+++ a1/sketch.js
@@ -14,7 +14,7 @@ function draw() {
   background(220);
  fill("yellow");
   circle(width/2,height/2,100);
-  stroke("green");
+  stroke("red");
   line(width/2,488,width/2,750);
   line(width/2,510,width/2-100,550);
   line(width/2,510,width/2+100,550);
@@ -24,4 +24,10 @@ function draw() {
   fill("white");
   circle(width/2-20,height/2-10,10);
   circle(width/2+20,height/2-10,10);
+  stroke("black");
+  fill("black");
+  rect(width/2-50,height/2-50,100,10);
+  rect(width/2-35,height/2-150,71,100);
+  fill("white");
+  rect(width/2-35,height/2-60 ,71,10)
 }
