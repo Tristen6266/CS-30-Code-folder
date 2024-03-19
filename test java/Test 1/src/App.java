2399183a493a73b110a09dcad05e921720f510f5
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-03-19 12:01:55 -0600

    assignment 1

---
+++ test java/Test 1/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to CS 30");
+        System.out.println("My name is Tristen and my favorite food is pizza,. \nJava has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
+        System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+        System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+        
+    }
+}
