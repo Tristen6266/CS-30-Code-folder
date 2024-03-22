6d406fef8bac7a5292fde1f7370c1a77aedb2617
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-03-22 12:06:10 -0600

    wing cost

---
+++ wings/Wing price/src/App.java
@@ -0,0 +1,10 @@
+// Wing Price
+// Tristen 
+// 3/22/2024
+public class App {
+    public static void main(String[] args) throws Exception {
+        double p= Math.floor(4.75/0.75);
+        int a=(int)p;
+        System.out.println("You can buy "+ a +" wings");
+    }
+}
