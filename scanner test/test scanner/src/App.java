39565a47afa56465cb0ceb580630374077149dd6
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-03-25 12:06:58 -0600

    scanner

---
+++ scanner test/test scanner/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner keys = new Scanner(System.in);
+        //Scanner keys2 =new Scanner(System.in);
+        // System.out.println("Scanner Demo Inputs");
+        
+        // System.out.println("Integers:");
+        // int num= keys.nextInt();
+        // System.out.println("The Number Was:"+ num);
+
+        // System.out.println("Double:");
+        // double dblnum= keys.nextDouble();
+        // System.out.println("The Number Was:"+ dblnum);
+
+        // System.out.println("Word:");
+        // String word= keys.nextLine();
+        // System.out.println("The Word Was:"+ word);
+
+        System.out.print("Enter an integer: ");
+        int num = keys.nextInt();
+        keys.nextLine();
+        System.out.print("Enter a Sentence: ");
+        String sentence = keys.nextLine();
+        System.out.println(num + " " + sentence);
+    }
+}
