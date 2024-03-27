74ed8959cca564bb1eb612b0715cac6c00d1a854
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-03-27 11:52:46 -0600

    strings

---
+++ string basics/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String messeage1= "I am very happy";
+        String target1 = "very";
+
+        String message2="That was great -lol";
+        String target2= "lol";
+
+       int x=messeage1.indexOf(target1);
+       System.out.println(messeage1.substring(0,x)+ messeage1.substring(10));
+
+       System.out.println(message2.replace(target2, " laugh out loud"));
+
+    }
+}
