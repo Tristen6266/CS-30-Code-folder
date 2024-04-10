26720944cde825999ac885e1366876d71d7e44fa
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-04-10 10:51:15 -0600

    Assignment 9

---
+++ Assinment 9/src/BigOrSmall.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class BigOrSmall {
+    
+        public void test(){
+        Scanner keys = new Scanner(System.in);
+       
+        System.out.print("Enter a number for A: ");
+         double a = keys.nextDouble();
+        keys.nextLine();
+        System.out.print("Enter another number for B: ");
+         double b = keys.nextDouble();
+         keys.nextLine();
+
+         if(a>b){
+            System.out.println("Yes");
+         }
+         else if(a<b){
+            System.out.println("No");
+         }
+         else{
+            System.out.println("Equals");
+         }
+    
+    }
+}
+
+
---
+++ Assinment 9/src/BigOrSmallRunner.java
@@ -0,0 +1,8 @@
+public class BigOrSmallRunner {
+    public static void main(String[] args) throws Exception {
+        BigOrSmall run = new BigOrSmall();
+        run.test();
+
+
+}
+}
