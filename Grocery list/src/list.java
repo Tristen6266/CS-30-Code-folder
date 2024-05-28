b55567eb5c9d276d4e40e6fece62127df6268839
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-05-28 11:26:10 -0600

    start

---
+++ Grocery list/src/list.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class list {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list =new ArrayList<String>();
+      for(i = 0; i < 1;){
+        Scanner keyboard= new Scanner(System.in);
+        System.out.println("what would you like to add to your list ");
+        String newlist = keyboard.nextLine();
+
+      }
+
+        }
+        
+    }
+
